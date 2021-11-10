6da2ec295abf6dec0019f7084b24c8913e026f3d
Abdulmumin Akinde <lanreywaju97@gmail.com>
2021-11-11 00:44:37 +0100

    add views and templates

--- polls/urls.py
+++ polls/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 
+app_name = 'polls'
 # Define your urlpatterns here
 urlpatterns = [
-  path('', views.index, name='index')
+  path('', views.index, name='index'),
+  path('<int:question_id>/', views.question_detail, name='detail')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.http import Http404
+from .models import Question
 
 # Create your views here.
-def index(reqquest):
-  pass
+def index(request):
+  latest_question_list = Question.objects.order_by('-pub_date')
+  context = {'latest_question_list': latest_question_list}
+  return render(request, 'polls/index.html', context)
+
+def question_detail(request, question_id):
+  try:
+    question = Question.objects.get(pk=question_id)
+  except Question.DoesNotExist:
+    raise Http404('Question does not exist')
+  return render(request, 'polls/detail.html', {'question': question})
