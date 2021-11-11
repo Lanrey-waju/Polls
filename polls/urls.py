0fef7bedbf87219dac1ac3203b24325971801bca
Abdulmumin Akinde <lanreywaju97@gmail.com>
2021-11-12 00:53:55 +0100

    add vote view

--- polls/urls.py
+++ polls/urls.py
@@ -6,5 +6,6 @@ app_name = 'polls'
 # Define your urlpatterns here
 urlpatterns = [
   path('', views.index, name='index'),
-  path('<int:question_id>/', views.question_detail, name='detail')
+  path('<int:question_id>/', views.question_detail, name='detail'),
+  path('<int:question_id>/', views.vote, name='vote')
 ]
